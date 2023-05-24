142719d864db011049afc0fd4276ff748bdb7ac2
samtaitai <slee550@myseneca.ca>
2023-05-24 17:37:50 -0400

    counters not working

--- WS02/w2p2/Timer.cpp
+++ WS02/w2p2/Timer.cpp
@@ -1,4 +1,27 @@
 #include "Timer.h"
 
+using Clock = std::chrono::high_resolution_clock;
+using TimePoint = std::chrono::time_point<Clock>;
+
+//citation: https://cplusplus.com/reference/chrono/duration/duration/
+//https://stackoverflow.com/questions/57538507/how-to-convert-stdchronoduration-to-double-seconds
+
 namespace sdds {
+
+	void Timer::start()
+	{
+		auto start = std::chrono::steady_clock::now();
+		m_timestamp = start;
+	}
+	long long Timer::stop()
+	{
+		long long result;
+		//# of ticks is represented in long long & tick period is nanoseconds 
+		typedef std::chrono::duration<long long, std::ratio<1, 1000000000>> nanoseconds_type;
+		
+		auto end = std::chrono::steady_clock::now();
+		nanoseconds_type diff = end - m_timestamp;
+		result = diff.count();
+		return 0;
+	}
 }
--- WS02/w2p2/Timer.h
+++ WS02/w2p2/Timer.h
@@ -1,11 +1,15 @@
 #pragma once
 #include <chrono>
+#include <ctime>
+#include <iostream>
 
 namespace sdds {
 	class Timer
 	{
+		std::chrono::time_point<std::chrono::high_resolution_clock> m_timestamp;
 	public:
-
+		void start();
+		long long stop();
 	};
 }
 

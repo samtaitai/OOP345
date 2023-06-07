5706e08fbccd0fccab3eea9ef2fca42558fadbdf
samtaitai <slee550@myseneca.ca>
2023-06-06 20:44:53 -0400

    ConfirmationSender ing...

--- WS04/w4p2/ConfirmationSender.cpp
+++ WS04/w4p2/ConfirmationSender.cpp
@@ -3,76 +3,128 @@
 
 using namespace std;
 namespace sdds {
-	//ostream& operator<<(std::ostream& os, ConfirmationSender& Ro)
-	//{
-	//	return Ro.display(os);
-	//}
-	//ConfirmationSender::~ConfirmationSender()
-	//{
-	//	delete[] m_ppReservations;
-	//}
-	//ConfirmationSender::ConfirmationSender(const ConfirmationSender& Ro)
-	//{
-	//	*this = Ro;
-	//}
-	//ConfirmationSender& ConfirmationSender::operator=(const ConfirmationSender& Ro)
-	//{
-	//	if (this != &Ro) {
-	//		delete[] m_ppReservations;
-	//		for (int i = 0; i < Ro.size(); i++) {
-	//			
-	//		}
-	//	}
-	//	return *this;
-	//}
-	//ConfirmationSender::ConfirmationSender(ConfirmationSender&& Ro)
-	//{
-	//	*this = std::move(Ro);
-	//}
-	//ConfirmationSender& ConfirmationSender::operator+=(const Reservation& res)
-	//{
-	//	/*
-	//	if the address of res is already in the array, this operator does nothing
-	//	resizes the array to make room for res if necessary
-	//	stores the address of res in the array (your function should not make copies of the reservation itself)
-	//	*/
-	//	bool found{};
-	//	for (int i = 0; i < m_cnt; i++) {
-	//		for (int j = 0; j < m_ppReservations[i].size(); j++) {
-	//			if(m_ppReservations[i][j] == &res) found = true;
-	//		}
-	//	}
+	ostream& operator<<(std::ostream& os, ConfirmationSender& Ro)
+	{
+		return Ro.display(os);
+	}
+	ConfirmationSender::~ConfirmationSender()
+	{
+		delete[] m_ppReservations;
+	}
+	ConfirmationSender::ConfirmationSender(const ConfirmationSender& Ro)
+	{
+		*this = Ro;
+	}
+	ConfirmationSender& ConfirmationSender::operator=(const ConfirmationSender& Ro)
+	{
+		if (this != &Ro) {
+			delete[] m_ppReservations;
+			m_ppReservations = new const Reservation * [Ro.size()];
+			for (int i = 0; i < Ro.size(); i++) {
+				m_ppReservations[i] = Ro.m_ppReservations[i];
+			}
+			m_cnt = Ro.m_cnt;
+		}
+		return *this;
+	}
+	ConfirmationSender::ConfirmationSender(ConfirmationSender&& Ro) noexcept
+	{
+		*this = std::move(Ro);
+	}
+	ConfirmationSender& ConfirmationSender::operator=(ConfirmationSender&& Ro) noexcept
+	{
+		if (this != &Ro) {
+			delete[] m_ppReservations;
+			m_ppReservations = Ro.m_ppReservations;
+			m_cnt = Ro.m_cnt;
+			Ro.m_ppReservations = nullptr;
+			Ro.m_cnt = 0;
+		}
+		return *this;
+	}
+	ConfirmationSender& ConfirmationSender::operator+=(const Reservation& res)
+	{
+		/*
+		if the address of res is already in the array, this operator does nothing
+		resizes the array to make room for res if necessary
+		stores the address of res in the array (your function should not make copies of the reservation itself)
+		*/
+		bool found{};
+		for (int i = 0; i < m_cnt; i++) {
+			if (m_ppReservations[i] == &res) found = true;
+		}
 
-	//	if (!found) {
-	//		//resize
-	//		
-	//		delete[] m_ppReservations;
-	//		m_ppReservations = new Restaurant[m_cnt + 1];
+		if (!found) {
+			const Reservation** temp{};
+			temp = new const Reservation * [m_cnt + 1];
+			
+			for (int i = 0; i < m_cnt; i++) {
+				temp[i] = m_ppReservations[i];
+			}
+			temp[m_cnt] = &res;
 
-	//		//store res
-	//	}
-	//	return *this;
-	//}
-	//size_t ConfirmationSender::size() const
-	//{
-	//	return m_cnt;
-	//}
-	//ostream& ConfirmationSender::display(std::ostream& os) const
-	//{
-	//	os << "--------------------------" << endl;
-	//	os << "Confirmations to Send" << endl;
-	//	os << "--------------------------" << endl;
-	//	if (m_cnt == 0) {
-	//		os << "There are no confirmations to send!" << endl;
-	//	}
-	//	else {
-	//		for (int i = 0; i < m_cnt; i++) {
-	//			//m_ppReservations[i] is array of Restaurant*
-	//			for (int j = 0; j < m_ppReservations[i].size(); j++) {
-	//				m_ppReservations[i].display(os);
-	//			}
-	//		}
-	//	}
-	//	return os;
-	//}
+			m_cnt++;
+			delete[] m_ppReservations;
+			m_ppReservations = new const Reservation * [m_cnt];
+
+			for (int i = 0; i < m_cnt; i++) {
+				m_ppReservations[i] = temp[i];
+			}
+			delete[] temp;
+		}
+		return *this;
+	}
+	ConfirmationSender& ConfirmationSender::operator-=(const Reservation& res)
+	{
+		bool found{};
+		size_t foundIdx{};
+
+		for (int i = 0; i < m_cnt; i++) {
+			if (m_ppReservations[i] == &res) {
+				found = true;
+				foundIdx = i;
+			}
+		}
+
+		if (found) {
+			const Reservation** temp{};
+			temp = new const Reservation * [m_cnt - 1];
+
+			for (int i = 0; i < m_cnt; i++) {
+				if (i != foundIdx) {
+					temp[i] = m_ppReservations[i];
+				}
+			}
+
+			m_cnt--;
+			delete[] m_ppReservations;
+			m_ppReservations = new const Reservation * [m_cnt];
+
+			for (int i = 0; i < m_cnt; i++) {
+				m_ppReservations[i] = temp[i];
+			}
+			delete[] temp;
+		}
+		return *this;
+	}
+	size_t ConfirmationSender::size() const
+	{
+		return m_cnt;
+	}
+	ostream& ConfirmationSender::display(std::ostream& os) const
+	{
+		os << "--------------------------" << endl;
+		os << "Confirmations to Send" << endl;
+		os << "--------------------------" << endl;
+		if (m_cnt == 0) {
+			os << "There are no confirmations to send!" << endl;
+		}
+		else {
+			for (int i = 0; i < m_cnt; i++) {
+				m_ppReservations[i]->display(os);
+			}
+		}
+		os << "--------------------------" << endl;
+		return os;
+	}
 }
--- WS04/w4p2/ConfirmationSender.h
+++ WS04/w4p2/ConfirmationSender.h
@@ -3,23 +3,24 @@
 #include "Reservation.h"
 
 namespace sdds {
-	/*class ConfirmationSender
+	class ConfirmationSender
 	{
-		const Restaurant* m_ppReservations{}; 
+		const Reservation** m_ppReservations{}; 
 		size_t m_cnt{};
 
 	public:
+		ConfirmationSender() = default;
 		~ConfirmationSender();
 		ConfirmationSender(const ConfirmationSender& Ro);
 		ConfirmationSender& operator=(const ConfirmationSender& Ro);
-		ConfirmationSender(ConfirmationSender&& Ro);
-		ConfirmationSender& operator=(ConfirmationSender&& Ro);
+		ConfirmationSender(ConfirmationSender&& Ro) noexcept;
+		ConfirmationSender& operator=(ConfirmationSender&& Ro) noexcept;
 		ConfirmationSender& operator+=(const Reservation& res);
 		ConfirmationSender& operator-=(const Reservation& res);
 		size_t size() const;
 		std::ostream& display(std::ostream& os) const;
 	};
-	std::ostream& operator<<(std::ostream& os, ConfirmationSender& Ro);*/
+	std::ostream& operator<<(std::ostream& os, ConfirmationSender& Ro);
 
 }
 

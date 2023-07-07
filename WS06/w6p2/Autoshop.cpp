7ac95bf7be8d6216919adbd102a87365bd97da4b
samtaitai <slee550@myseneca.ca>
2023-07-07 10:47:29 -0400

    declarations

--- WS06/w6p2/Autoshop.cpp
+++ WS06/w6p2/Autoshop.cpp
@@ -1,3 +1,11 @@
+/*
+* Name: Soyon Lee
+* Email: slee550@myseneca.ca
+* ID: 179142211
+* Date: 7 July 2023
+* I have done all the coding by myselfand only copied the code that
+* my professor provided to complete my workshopsand assignments.
+*/
 #include <iostream>
 #include "Autoshop.h"
 #include "Car.h"

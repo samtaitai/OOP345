0720efdc26a835bde7710f6f8d27a8e113fb77dd
samtaitai <slee550@myseneca.ca>
2023-07-08 17:25:01 -0400

    utility done

--- milestone/Utilities.cpp
+++ milestone/Utilities.cpp
@@ -18,21 +18,63 @@ namespace sdds {
 
 	std::string Utilities::extractToken(const std::string& str, size_t& next_pos, bool& more)
 	{
+		size_t start = next_pos;
+		size_t end = str.find(m_delimiter, next_pos);
 		std::string result{};
-		std::stringstream row(str);
 
-		if (str[next_pos] == m_delimiter) { //Hello ll situation
+		if (end == start) {
 			more = false;
 			throw "   ERROR: No token.\n";
 		}
-		else {
-			std::getline(row, result, m_delimiter);
-			result = trim(result);
-			next_pos++;
-			if (getFieldWidth() < result.length()) setFieldWidth(result.length());
-			if (!str[next_pos]) more = false;
-			return result;
+		else if (end == std::string::npos) {
+			end = str.length();
+			start = end;
+
+			if (end == start) {
+				more = false;
+				//throw "   ERROR: No token.\n";
+				result = str.substr(next_pos);
+				if (getFieldWidth() < result.length()) setFieldWidth(result.length());
+				return trim(result);
+			}
+
 		}
+		else {
+
+			
+			next_pos = end + 1;
+			
+			result = str.substr(start, end - start);
+			if (getFieldWidth() < result.length()) setFieldWidth(result.length());
+			return trim(result);
+		}
+		
+
+		
+
+		//std::string result{};
+		//std::string temp = str.substr(next_pos);
+		//std::stringstream row(temp);
+		//std::string newStr{};
+
+		////"apple,orange,banana,kiwi,strawberry,yellow watermellon"
+
+		//if (row.peek() == m_delimiter) { //Hello ll situation
+		//	more = false;
+		//	throw "   ERROR: No token.\n";
+		//}
+		//else {
+		//	std::getline(row, result, m_delimiter);
+		//	if (result.length() > 0) {
+		//		result = trim(result);
+		//		next_pos = next_pos + result.length();
+		//		if (getFieldWidth() < result.length()) setFieldWidth(result.length());
+		//		if (!temp[next_pos]) more = false;
+		//		return result;
+		//	}
+		//	else more = false;
+		//	
+		//}
 	}
 
 	void Utilities::setDelimiter(char newDelimiter)
